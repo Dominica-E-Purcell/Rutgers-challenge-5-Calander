391e3c4bfa25aac67c3032372596549bf48cf636
Dominica-E-Purcell <dominica.e.purcell@gmail.com>
2023-03-27 22:48:34 -0400

    moved extra code/comments  html/js to gitignore

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,32 +1,57 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
-    /*TODO: Add a listener for click events on the save button. This code should
-     use the id in the containing time-block as a key to save the user input in
-    local storage. HINT: What does `this` reference in the click listener
-    function? How can DOM traversal be used to get the "hour-x" id of the
-    time-block containing the button that was clicked? How might the id be
-    useful when saving the description in local storage?
-    */
+   var calenderListElementTime = $('#hour')
+   var calenderListElementText = $('#input-text')
+   var calenderListElementSaveButton = $('#save-btn')
+   var timeDisplay = $('#currentDay')
+   
+   calenderListElementText.textarea('').addClass('col-8 col-md-10', 'description', 'row-3')
+   calenderListElementTime.attr('row', 'time-block').addClass('col-2 col-md-1', 'text-center', 'p-4');
+   calenderListElementSaveButton.attr('<button></button>').addClass('col-2 col-md-1')
+   timeDisplay.attr(dayjs().format('dddd, MMMM D YYYY, h:mm:ss'));
+
+
+function () {
+    var calenderListElementTime = [
+        9 + 'AM',
+        10 + 'AM',
+        11 + 'AM',
+        12 + 'PM',
+        1 + 'PM',
+        2 + 'PM',
+        3 + 'PM',
+        4 + 'PM',
+        5 + 'PM',
+      ];
+    for (var i = 0; i < calenderListElementTime.length; i++) {
+        var calenderListElementTime = $("<main>", '<li>' + abilities[i] + '</li>', "</main>");
+      }
     
+      if (calenderListElementTime === dayjs().format('h: a')()) {
+        calenderListElementText.addClass('present');
+      } else if (calenderListElementTime <= dayjs().format('h: a')()) {
+        calenderListElementText.addClass('past');
+    } else (calenderListElementTime >= dayjs().format('h: a')()) {
+            calenderListElementText.addClass('future'); 
+    }
+)};
+    
+      
+
+
+
 
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
-    
-    /* TODO: Add code to apply the past, present, or future class to each time
-    block by comparing the id to the current hour. HINTS: How can the id
-    attribute of each time-block be used to conditionally add or remove the
-    past, present, and future classes? How can Day.js be used to get the
-    current hour in 24-hour time?
-    */
-    
-    /* TODO: Add code to get any user input that was saved in localStorage and set
-     the values of the corresponding textarea elements. HINT: How can the id
-     attribute of each time-block be used to do this?
-    */
-    
-     // TODO: Add code to display the current date in the header of the page.
-     
-
-  
+   
